0cfc88151441e2ab0223f94e9166792e99551199
Mark Snyder <lightsquire@gmail.com>
2018-06-26 13:45:51 -0400

    Initial Commit

---
+++ GitExampleXC9/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitExampleXC9
+//
+//  Created by Mark Snyder on 6/26/18.
+//  Copyright © 2018 Mark Snyder. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
